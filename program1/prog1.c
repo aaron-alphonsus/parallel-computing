e6bb55ca8f133e3dd4c4f8d88315135acdb79892
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2018-09-29 17:34:46 -0600

    Added timing and scheduling.

--- program1/prog1.c
+++ program1/prog1.c
@@ -3,10 +3,11 @@
 
 #include <omp.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 /* Check if a given input produces TRUE (a one) */
-int check_circuit (int id, int z)
+int check_circuit (int id, int z, bool print)
 {
     int v[16]; /* Each element is a bit of z */
     int i;
@@ -14,7 +15,7 @@ int check_circuit (int id, int z)
     for (i = 0; i < 16; i++) 
         v[i] = EXTRACT_BIT(z,i);
     
-    if ((v[0] || v[1]) && (!v[1] || !v[3]) && (v[2] || v[3])
+    if (((v[0] || v[1]) && (!v[1] || !v[3]) && (v[2] || v[3])
         && (!v[3] || !v[4]) && (v[4] || !v[5])
         && (v[5] || !v[6]) && (v[5] || v[6])
         && (v[6] || !v[15]) && (v[7] || !v[8])
@@ -23,6 +24,7 @@ int check_circuit (int id, int z)
         && (v[9] || v[11]) && (v[10] || v[11])
         && (v[12] || v[13]) && (v[13] || !v[14])
         && (v[14] || v[15])) 
+        && print) 
     {
         printf ("%d) %d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n", id,
             v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7],v[8],v[9],v[10],v[11],v[12],
@@ -37,17 +39,35 @@ int check_circuit (int id, int z)
 
 int main()
 {
+    int i, j;
     int n = pow(2, 16);    
-    int thread_count = 8; 
+    int thread_count = 4; 
+    // printf("%d, %d\n", omp_get_num_procs(), omp_get_max_threads());
 
-    # pragma omp parallel for num_threads(thread_count)
-    for(int i = 0; i < n; i++)
-    {    
-        int id = omp_get_thread_num();
-        // printf("%d: %d\n", i, check_circuit(i, i));
-        // printf("%d\n", id);
-        check_circuit(id, i);
+    int reps = 10;
+    bool print = false;
+    
+    double begin, end;
+    double time = 0.0;
+    
+    for(i = 0; i < reps; i++)                                                           
+    {                                                                             
+        begin = omp_get_wtime(); 
+        # pragma omp parallel for num_threads(thread_count) \
+            default(none) reduction(+: time) private(i) shared(n, print) \
+            schedule(static, 1) 
+        for(j = 0; j < n; j++)
+        {    
+            int id = omp_get_thread_num();
+            check_circuit(id, j, print);
+            
+            // printf("%d: %d\n", j, check_circuit(j, j));
+            // printf("%d\n", id);
+        }
+        end = omp_get_wtime();
+        time += (double)((end-begin)*1000.0);
     }
+    printf("Time %.4lf ms\n", (double)time / reps);
 
     return 0;
 }
