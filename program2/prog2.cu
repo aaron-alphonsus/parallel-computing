e2678006075a35cf4baa9c0da7d05664efadb8b6
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2018-11-13 16:03:28 -0700

    Added program documentation.

--- program2/prog2.cu
+++ program2/prog2.cu
@@ -1,15 +1,18 @@
-/* File:                                                                         
+/*------------------------------------------------------------------------------
+ * File:
  *    prog2.cu
  *                                                                               
  * Purpose:                                                                      
- *    TODO A brute force solution to a circuit-satisfiability question parallelizing
- *    the algorithm using OpenMP. Compares time taken by parallel and serial 
- *    approaches.     
+ *    Host program and CUDA kernel to multiply a square (n x n) matrix with a
+ *    vector of length n. Each row of the square matrix is initialized from 1 to
+ *    and the vector is also initialized from 1 to n. The resultant vector
+ *    contains the sum of squares from 1 to n.
  *                                                                               
  * Input:                                                                        
- *    TODO none (pre-defined circuit in the form of an if statement)                            
+ *    none (Matrix and vector initialized to yield sum of squares as result)
  * Output:                                                                       
- *    TODO All combinations of inputs that satisfy the circuit.
+ *    The matrix multiplication result along with some general information for
+ *    debugging purposes
  *                                                                               
  * Compile:
  *    nvcc -o prog2 prog2.cu
@@ -27,25 +30,56 @@
  */
 
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <math.h>
 
+/*------------------------------------------------------------------------------
+ * Function: matvecMul
+ *
+ * Purpose:  CUDA kernel code that executes the matrix-vector multiplication on
+ *           the GPU. We have gone with coarse-grained parallelism here: each
+ *           thread multiplies a row of A with B serially.
+ *
+ * In args:  A, B, C, n
+ *
+ * Out args: C
+ *
+ * Returns:  void
+ */
 __global__ void matvecMul(double *A, double *B, double *C, int n)
 {
+    // Declare variable for dot-product summation
     double sum = 0;
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    int stride = blockDim.x * gridDim.x;
     
+    // Threads to index into each row of the square matrix
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x; // grid-stride
+
+    // A grid-stride loop where each thread multiplies a row of A with B in
+    //  parallel
     for(int i = index; i < n; i += stride)
     {
         sum = 0; 
+
+        // Sequential portion: each thread calculates the dot-product summation
+        //  serially
         for(int j = 0; j < n; j++)
             sum += A[i * n + j] * B[j];
+
         C[i] = sum;
     }
 }
 
-int main(int argc, char* argv[])
+/*------------------------------------------------------------------------------
+ * Function: main
+ *
+ * Purpose:  Declares vectors and allocates unified memory to them. Initializes
+ *           the vector B and each row of the matrix A with numbers from 1 to n.
+ *           Calls the CUDA kernel after defining grid and block sizes to
+ *           execute the multiplication in parallel. Prints out resultant vector
+ *           C.
+ *
+ * Returns:  0 indicating normal termination
+ */
+int main()
 { 
     // Size of vectors
     int n = 8192;
@@ -93,16 +127,15 @@ int main(int argc, char* argv[])
     int gridSize = (int)ceil((float)n/blockSize);
     // Execute the kernel
     matvecMul<<<gridSize, blockSize>>>(A, B, C, n);
-    // matvecMul<<<1, 128>>>(A, B, C, n);
-
-    // matvecMul_serial(A, B, C, n);
 
     // Wait for the GPU to finish
     cudaDeviceSynchronize();
-  
+
+    // Calculate sum of squares
     for(int i = 0; i < n; i++)
         sumofsq += (i + 1) * (i + 1);
-    
+
+    // Print out matrix C
     printf("Matrix C\n");
     for(int i = 0; i < n; i++) 
         printf("%0.1lf ", C[i]);
